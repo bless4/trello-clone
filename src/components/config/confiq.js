efab96fe9a910a7fee33d0f7ec8d63015fa7433a
Martin Sone <kwangmart12@gmail.com>
2020-11-15 15:13:43 +0100

    code base changed added sidebar.js topbar.js

---
+++ src/components/config/confiq.js
@@ -0,0 +1,4 @@
+const confiq = {
+	url: 'https//api.unsplash.com',
+	clientKey:'pYasIpNC7V7QoSYv8dPtMbwcMvjoPKJ9wZExvAxsQ1Q',
+}
--- src/components/input/InputContainer.js
+++ src/components/input/InputContainer.js
@@ -1,40 +1,42 @@
-
-import React, {useState} from 'react';
-import { Paper, Collapse } from '@material-ui/core';
-import {fade, makeStyles} from '@material-ui/core/styles';
-import {Typography} from '@material-ui/core';
+import React, { useState } from 'react';
+import { Paper, Typography, Collapse } from '@material-ui/core';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import InputCard from './InputCard';
 
-const useStyle = makeStyles((theme) =>({
-         root:{
-             marginTop:theme.spacing(2),
-         },
-    addCard: {
-        padding:theme.spacing(1, 1, 1, 2), 
-        margin:theme.spacing(0, 1, 1, 1),
-        background:'#EBECF0',
-        "&:hover":{
-            backgroundColor:fade('#000',0.25),
-        }
-     },
- }));
+const useStyle = makeStyles((theme) => ({
+  root: {
+    width: '300px',
+    marginTop: theme.spacing(1),
+  },
+  addCard: {
+    padding: theme.spacing(1, 1, 1, 2),
+    margin: theme.spacing(0, 1, 1, 1),
+    background: '#EBECF0',
+    '&:hover': {
+      backgroundColor: fade('#000', 0.25),
+    },
+  },
+}));
 
-const InputContainer = () => {
-    const classes= useStyle();
-    const [open, setOpen] = useState(false);
-    return (
-        <div className={classes.root}>
-            <Collapse in={open}>
-            <InputCard setOpen={setOpen}/>
-            </Collapse>
-           <Collapse in={!open}>
-           <Paper className={classes.addCard} elevation={0} onClick={()=> setOpen(!open)}>
-            <Typography>+ Add a Card</Typography>
-        </Paper> 
-           </Collapse>
-        
-        </div>
-    );
-};
-
-export default InputContainer;
+export default function InputContainer({ listId, type }) {
+  const classes = useStyle();
+  const [open, setOpen] = useState(false);
+  return (
+    <div className={classes.root}>
+      <Collapse in={open}>
+        <InputCard setOpen={setOpen} listId={listId} type={type} />
+      </Collapse>
+      <Collapse in={!open}>
+        <Paper
+          className={classes.addCard}
+          elevation={0}
+          onClick={() => setOpen(!open)}
+        >
+          <Typography>
+            {type === 'card' ? '+ Add a Card' : '+ Add another List'}
+          </Typography>
+        </Paper>
+      </Collapse>
+    </div>
+  );
+}
